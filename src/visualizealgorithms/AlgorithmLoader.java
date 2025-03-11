55d3f10e56d1358980fc9826a8899a3fe750920d
GreenzQe <73607229+GreenzQe@users.noreply.github.com>
2025-03-11 11:18:07 +0100

    Comments

--- src/visualizealgorithms/AlgorithmLoader.java
+++ src/visualizealgorithms/AlgorithmLoader.java
@@ -16,15 +16,15 @@ public class AlgorithmLoader {
     private ArrayList<IAlgorithm> algorithms = new ArrayList<>();
 
     public AlgorithmLoader() {
-        //Modify default inputs
-        inputs = "1000;2000;4000;8000;16000;32000;64000;128000;256000;512000;1024000;2048000;4096000;8192000;16384000;32768000;65536000;131072000;262144000;524288000;1048576000";
+        //Modify default inputs - Default is: inputs = "1000;2000;4000;8000;16000;32000;64000";
+        inputs = "1000;2000;4000;8000;16000;32000;64000;128000;256000;512000;1024000;2048000;4096000;8192000;16384000;32768000;65536000;131072000";
 
         //Add implemented/selected algorithms here..
-        algorithms.add(new BubbleSort());
-        algorithms.add(new RecombinantSort());
-        algorithms.add(new QuickSort());
-        algorithms.add(new InsertionSort());
-        algorithms.add(new SelectionSort());
+        algorithms.add(new BubbleSort()); // O(n^2)
+        algorithms.add(new RecombinantSort()); // O(n)
+        algorithms.add(new QuickSort()); // O(n log n)
+        algorithms.add(new InsertionSort()); // O(n^2)
+        algorithms.add(new SelectionSort()); // O(n^2)
 
         //more algorithms here :)
     }
